bfdffda0228d194c1f399876b5565117917a176e
Vikas Rajput <39953644+davidthakur@users.noreply.github.com>
2023-06-15 12:38:12 +0530

    Update OtherCity.js

--- src/Components/OtherCity.js
+++ src/Components/OtherCity.js
@@ -51,7 +51,7 @@ export default function OtherCity() {
    
     ) : (
         <div
-          className="w-[80%] mt-7 h-[80vh] lg:w-[40%] 2xl:w-[50%] bg-black flex flex-col  items-center  rounded-xl shadow-xl   shadow-pink-700"
+          className="w-[95%] mt-7 h-[80vh] lg:w-[40%] 2xl:w-[50%] bg-black flex flex-col  items-center  rounded-xl shadow-xl   shadow-pink-700"
           style={{
             backgroundImage:
               "url(https://media.tenor.com/9vRAkntogEMAAAAd/background.gif)",
@@ -66,7 +66,7 @@ export default function OtherCity() {
               }}
               id="input"
               type="text"
-              className="w-56 text-2xl font-serif h-14 placeholder:text-center ml-7 placeholder:text-cyan-400 text-orange-600 text-center outline-none rounded-xl"
+              className="w-44 text-2xl font-serif h-8 sm:w-40 sm:h-10 placeholder:text-center ml-7 placeholder:text-cyan-400 text-orange-600 text-center outline-none rounded-xl"
             placeholder="Enter City "
             />
 
@@ -76,7 +76,7 @@ export default function OtherCity() {
                 setCity(input);
                 navigate();
               }}
-              className="w-12 cursor-pointer animation duration-100 hover:scale-125 ml-4"
+              className="w-8 sm:w-10 cursor-pointer animation duration-100 hover:scale-125 ml-4"
               src="map.png"
               alt=''
             />
@@ -84,94 +84,95 @@ export default function OtherCity() {
 
           <div className="w-[100%] h-[10vh]  flex justify-center items-center flex-col">
           <div className="flex justify-between w-[18%] items-center mt-5">
-            <img className="w-8" src="cityscape-svgrepo-com.svg" alt="" />
-            <h1 className="text-white font-serif text-2xl">{Location.name}</h1>
+            <img className="w-8 " src="cityscape-svgrepo-com.svg" alt="" />
+            <h1 className="text-white font-serif text-lg lg:text-2xl ml-3">{Location.name}</h1>
 
             </div>
-            <h1 className="text-white font-serif text-xl mt-4">
+            <h1 className="text-white font-serif text-sm lg:text-lg mt-4">
               {Location.region},({Location.country})
             </h1>
           </div>
           <div className="w-[100%] h-[10vh]  flex justify-center items-center flex-col">
             <img
-              className="w-20 cursor-pointer  "
+              className="sm:w-16 lg:w-16 mt-4  w-12 cursor-pointer  "
               src={icon}
               alt=""
             />
           </div>
           <div className="w-[100%] h-[10vh]  flex justify-center items-center flex-col">
-            <h1 className="text-white font-mono text-6xl">
+            <h1 className="text-white font-mono mt-4 text-xl sm:text-4xl xl:text-5xl">
               {Weather.temp_c}C°
             </h1>
             <div className=" flex justify-center items-center h-[100%] w-[100%]">
             <img className="w-10 cursor-pointer mt-4  " src='reshot-icon-weather-EJPNZMV8U5.svg' alt="" />
-            <h1 className="text-white font-mono ml-7 text-2xl mt-5 text-center">{Forecast}</h1>
+            <h1 className="text-white font-mono ml-3 text-md lg:text-xl mt-4 text-center">{Forecast}</h1>
             </div>
             {/* <h1 className="text-white font-mono text-2xl">{Forecast}</h1> */}
           </div>
 
-          <div className="w-[90%] h-[10vh] mt-10 rounded-xl  flex justify-center items-center flex-col bg-[#FFF8F068] shadow-2xl shadow-slate-300">
+          <div className="w-[100%] lg:w-[90%] h-[10vh] mt-10 rounded-xl  flex justify-center items-center flex-col bg-[#FFF8F068] shadow-2xl shadow-slate-300">
             <div className="flex w-[100%] h-[100%] justify-around">
-              <h1 className="text- font-serif text-lg">humidity</h1>
-              <h1 className="text-black font-serif text-lg">Temp F°</h1>
-              <h1 className="text-black font-serif text-lg">Pressure</h1>
-              <h1 className="text-black font-serif text-lg">Wind KMPH</h1>
+              <h1 className="text- font-serif text-sm lg:text-lg">humidity</h1>
+              <h1 className="text-black font-serif text-sm lg:text-lg">Temp F°</h1>
+              <h1 className="text-black font-serif text-sm lg:text-lg">Pressure</h1>
+              <h1 className="text-black font-serif text-sm lg:text-lg">Wind KMPH</h1>
             </div>
             <div className="flex w-[100%] h-[100%] justify-around">
-              <h1 className="text- font-serif text-lg">{Weather.humidity}</h1>
-              <h1 className="text-black font-serif text-lg">
+              <h1 className="text- font-serif text-sm lg:text-lg">{Weather.humidity}</h1>
+              <h1 className="text-black font-serif text-sm lg:text-lg">
                 {Weather.temp_f}
               </h1>
-              <h1 className="text-black font-serif text-lg">
+              <h1 className="text-black font-serif text-sm lg:text-lg">
                 {Weather.pressure_in}
               </h1>
-              <h1 className="text-black font-serif text-lg">
+              <h1 className="text-black font-serif text-sm lg:text-lg">
                 {Weather.wind_kph}
               </h1>
             </div>
           </div>
-          <div className="w-[90%] h-[10vh] mt-10 rounded-xl  flex justify-center items-center flex-col bg-[#FFF8F068] shadow-2xl shadow-slate-300">
+          <div className="w-[100%] sm:w-[90%] h-[10vh] mt-10 rounded-xl  flex justify-center items-center flex-col bg-[#FFF8F068] shadow-2xl shadow-slate-300">
             <div className="flex w-[100%] h-[100%] justify-around">
-              <h1 className="text- font-serif text-lg">Clouds</h1>
-              <h1 className="text-black font-serif text-lg">Pressure MB</h1>
-              <h1 className="text-black font-serif text-lg">Wind Degree</h1>
-              <h1 className="text-black font-serif text-lg">Wind Direc.</h1>
+              <h1 className="text- font-serif text-sm lg:text-lg">Clouds</h1>
+              <h1 className="text-black font-serif text-sm lg:text-lg">Pressure MB</h1>
+              <h1 className="text-black font-serif text-sm lg:text-lg">Wind Degree</h1>
+              <h1 className="text-black font-serif text-sm lg:text-lg">Wind Direc.</h1>
             </div>
             <div className="flex w-[100%] h-[100%] justify-around">
-              <h1 className="text- font-serif text-lg">{Weather.cloud}</h1>
-              <h1 className="text-black font-serif text-lg">
+              <h1 className="text- font-serif text-sm lg:text-lg">{Weather.cloud}</h1>
+              <h1 className="text-black font-serif text-sm lg:text-lg">
                 {Weather.pressure_mb}
               </h1>
-              <h1 className="text-black font-serif text-lg">
+              <h1 className="text-black font-serif text-sm lg:text-lg">
                 {Weather.wind_degree}
               </h1>
-              <h1 className="text-black font-serif text-lg">
-               {Weather.wind_dir}
+              <h1 className="text-black font-serif text-sm lg:text-lg">
+                {Weather.wind_dir}
               </h1>
             </div>
           </div>
         </div>
       )}
-      {loading ? (
+        {loading ? (
        
          
-       <></>
+          <></>
 
+        
      
-  
-   ) : (
-   <div className="w-[50%] h-[5vh] mt-10  flex justify-center items-center">
-     <Link to={"/Other"}>
-       <button className="px-4 py-2 rounded-xl hover:bg-orange-600 bg-yellow-500">
-         Other Cities
-       </button>
-     </Link>
-     <Link to={"/"}>
-       <button className="px-4 ml-10 py-2 rounded-xl hover:bg-orange-600 bg-green-500">
-         My Location
-       </button>
-     </Link>
-   </div>)}
+      ) : (
+      <div className="w-[50%] h-[5vh] mt-10  flex justify-center items-center">
+        <Link to={"/Other"}>
+          <button className=" w-28 h-10  sm:w-28 sm:h-12   rounded-xl hover:bg-orange-600 bg-yellow-500">
+            Other Cities
+          </button>
+        </Link>
+        <Link to={"/"}>
+          <button className=" w-28 h-10 sm:w-28 sm:h-12 ml-6  rounded-xl hover:bg-orange-600 bg-green-500">
+            My Location
+          </button>
+        </Link>
+      </div>)}
       </>
+  
   );
 }
