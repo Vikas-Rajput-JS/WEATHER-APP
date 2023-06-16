ccfc7f68049d34f27246f5fd7ef9ab63cc6bb6c7
Vikas Rajput <39953644+davidthakur@users.noreply.github.com>
2023-06-16 11:56:35 +0530

    and

--- src/Components/CurrentLoc.js
+++ src/Components/CurrentLoc.js
@@ -54,7 +54,7 @@ export default function CurrentLoc() {
           backgroundSize: "100% 100%",
         }}>
           <SyncLoader color="#36d7b7" />
-          <h1 className="text-4xl text-neutral-200 mt-10">Please Wait...</h1>
+          <h1 className="text-4xl text-neutral-200 font-serif mt-10">Please Wait...</h1>
         </div>
       ) : (
         <div
